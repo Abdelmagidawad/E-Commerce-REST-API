eb9dd867e485e0c1bff4cc8bf7d3977e5a5ec82d
Abdelmagidawad <abdelmagidawad@gmail.com>
2025-11-20 01:14:38 +0200

    using bcrypt backage

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,35 +1,61 @@
-import users from "../models/users.js";
+import users from "../models/usersModels.js";
+import bcrypt from "bcrypt";
 
 // @docs Register a new user
 // @route /auth/register
 // @method POST
-const registerUser = (req, res) => {
-  const { id, name, age, email, gender, password } = req.body;
-  users.push({ id, name, age, email, gender, password });
-  res.status(201).json({
-    success: true,
-    message: "Create new user",
-    users,
-  });
+const registerUser = async (req, res) => {
+  const { userName, age, email, gender, password } = req.body;
+  try {
+    const userExist = users.find((user) => user.email === email);
+    if (userExist) {
+      return res.status(400).json({ message: "Invalid this user" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    users.push({
+      id: users.length + 1,
+      userName,
+      age,
+      email,
+      gender,
+      password: hashedPassword,
+    });
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      users,
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
 // @docs Login user
 // @route /auth/login
 // @method POST
-const loginUser = (req, res) => {
+const loginUser = async (req, res) => {
   const { email, password } = req.body;
-  const user = users.find((user) => user.email === email);
-  if (user && user.password === password) {
-    res.status(200).json({
-      success: true,
-      message: "Login successfully",
-      user,
-    });
-  } else {
-    res.status(400).json({
-      success: false,
-      message: "email or password is wrong",
-    });
+  try {
+    const user = users.find((user) => user.email === email);
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Invalid email or password" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) {
+      res.status(200).json({
+        success: true,
+        message: "Login successfully",
+        user,
+      });
+    } else {
+      res
+        .status(401)
+        .json({ success: false, message: "Invalid email or password" });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 };
 
---
+++ models/usersModels.js
@@ -0,0 +1,16 @@
+// Data => database
+
+//password :123456
+
+let users = [
+  {
+    id: 1,
+    userName: "omar",
+    age: 24,
+    email: "omar@gmail.com",
+    gender: "Male",
+    password: "$2b$10$YHxfPHf6UehnedeLUvHnCO1CTGDPOUM/9tzX8xViYAxq4ljlThtuK",
+  },
+];
+
+export default users;
